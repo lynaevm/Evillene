dc4636fccb0360d2ffca9345e780a5e5968c15d5
lynaevm <lynaevm@aol.com>
2018-06-03 11:40:07 -0500

    Add files via upload

---
+++ Evillene2/Evillene2.ino
@@ -0,0 +1,221 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_CircuitPlayground.h>
+#define FASTLED_ALLOW_INTERRUPTS 0
+#include <FastLED.h>
+
+
+//#define LED_PIN     9    //led strand is soldered to pin 6
+#define NUM_LEDS_PER_STRIP 153
+
+#define COLOR_ORDER GRB
+#define NUM_MODES 3
+
+CRGB leds[NUM_LEDS_PER_STRIP];
+
+uint8_t brightness = 255;  //led strand brightness control
+uint8_t cpbrightness = 30;  //circuit playground brightness control
+
+int ledMode = -1;       //Initial mode 
+bool leftButtonPressed;
+bool rightButtonPressed;
+
+void State2(); // Forward declaration to other file.
+
+void setup() {
+  
+  Serial.begin(57600);
+  CircuitPlayground.begin();
+  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 9 >(leds, NUM_LEDS_PER_STRIP);
+  set_max_power_in_volts_and_milliamps(5, 2000);               // FastLED 2.1 Power management set at 5V, 500mA
+  FastLED.setBrightness( 200 );
+  //FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  
+}
+
+void loop()  {
+
+  leftButtonPressed = CircuitPlayground.leftButton();
+  rightButtonPressed = CircuitPlayground.rightButton();
+
+  if (leftButtonPressed) {  //left button cycles through modes
+
+    CircuitPlayground.clearPixels();
+    //State1 = 0, 127, 254
+    //State2 = 197, 104, 0
+    //State3 = 254, 0, 127
+    for(int i=0;i<5;i++) // change the 3 to however many pixels you want to show. 
+    CircuitPlayground.setPixelColor(i, 127 * (ledMode % 3), 127 * ((ledMode + 1) % 3), 127 * ((ledMode + 2) % 3));
+
+
+
+
+
+   
+    //CircuitPlayground.clearPixels();
+    ledMode = ledMode+1;
+    ledMode = ledMode % NUM_MODES;
+
+    
+    CircuitPlayground.setPixelColor(6, 0, 20, 35);
+    CircuitPlayground.setPixelColor(7, 0, 0, 125);
+    CircuitPlayground.setPixelColor(8, 0, 20, 35);
+   
+    FastLED.delay(1000); // long delay to avoid button "debouncing" for debugging.
+  }
+    if (rightButtonPressed) {   // right button turns all leds off
+      clearpixels();
+      ledMode=-1;
+    }
+  
+ switch (ledMode) {
+       case 0:  //state 1 code here;
+          State1();
+          break; 
+       case 1:  //state 2 code here; 
+          State2();
+          break;                    
+       case 2:  //state 3 code here;
+          State3();
+          break; 
+       default: 
+          Mode99();
+          break;      
+  }
+  //Note: If you want a variable delay based on mode, copy the delay code into the switch statement for each "case".
+  FastLED.delay(1);
+}
+
+void clearpixels()
+{
+  for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) leds[i] = CRGB::Black; 
+  FastLED.show();
+}
+
+void Mode99()
+{
+  // static int is a fancy way of saying "keep this value at the same space in memory so it persists
+  // between calls to the  method"
+  static int state = 0;
+  static int iterations = 0; 
+  const int state0iterations = 100; // we want the lights to be on for 20 * 100 ms (2 seconds on) 
+  const int state1iterations = 1000; // we want the lights to be off for 20 *1000 ms (20 seconds off) 
+
+  // this method must exit back to the main loop() method quickly. 
+  // outside this method call, in the parent loop is a delay(20) call. 
+  // we expect this to run roughly every 20-25ms. (because of execution time, variable time between iterations. 
+  // we could bypass this by using the millis() method call to get a more exact representation of time passed, but this should
+  // suffice for our needs. 
+
+  iterations++; 
+  switch(state)
+  {
+    case 0: 
+      if(iterations % state0iterations == 0) // % is the modulus operator. is basically says divide the two numbers and take the remainder.
+      {
+        // we are exiting the "on" state. turn off lights, restart counter.
+        state = 1;
+        iterations = 0; 
+        fill_solid( leds, NUM_LEDS_PER_STRIP, CRGB(0,0,0));
+        FastLED.show(); 
+      }
+      break;
+      case 1:
+      if(iterations % state1iterations == 0)
+      {
+        // we are exiting the "off" state. Turn on lights, and restart iteration counter. 
+       state = 0; 
+       iterations = 0; 
+       fill_solid( leds, 3, CRGB(124,252,0)); //shows lawn green 
+       FastLED.show(); 
+      }
+  }
+  
+  CircuitPlayground.clearPixels();
+  CircuitPlayground.setPixelColor(2, 0, 55, 0);
+  CircuitPlayground.setPixelColor(1, 0, 35, 0); 
+  CircuitPlayground.setPixelColor(3, 0, 35, 0);
+ } // end Mode99()
+
+// variables specific to Mode 0 
+float redStates[NUM_LEDS_PER_STRIP];
+float blueStates[NUM_LEDS_PER_STRIP];
+float greenStates[NUM_LEDS_PER_STRIP];
+float fadeRate = .98;
+
+void State1() {
+  // Each time we enter, 1/20 chance of "priming" a light
+  if (random(20) == 1) {
+    uint16_t i = random(NUM_LEDS_PER_STRIP);
+      if (redStates[i] < 1 && greenStates[i] < 1 && blueStates[i] < 1) { // Don't prime a light if its already lit. 
+        
+        redStates[i] = random(180);
+        greenStates[i] = random(256);
+        blueStates[i] = random(206);
+    }
+  }
+    for(uint16_t l = 0; l < NUM_LEDS_PER_STRIP; l++) {
+      if (redStates[l] > 1 || greenStates[l] > 1 || blueStates[l] > 1) {
+        if (redStates[l] > 1) {
+          redStates[l] = redStates[l] * fadeRate;
+        } else {
+          redStates[l] = 0;
+        }
+        
+        if (greenStates[l] > 1) {
+          greenStates[l] = greenStates[l] * fadeRate;
+        } else {
+          greenStates[l] = 0;
+        }
+        
+        if (blueStates[l] > 1) {
+          blueStates[l] = blueStates[l] * fadeRate;
+        } else {
+          blueStates[l] = 0;
+        }
+        CRGB newcolor;
+        // setRGB requires integer values for parameters. 
+        // cast to int to truncate float values. 
+        newcolor.setRGB( (int)redStates[l], (int)greenStates[l], (int)blueStates[l]);
+        leds[l] = newcolor;
+      }
+    }
+    FastLED.show();
+    // Note: We removed the delay here.
+    // All delays should be handled in the main loop() method.
+}
+
+// specific for Mode2
+#define COLORS 3
+
+void State3()
+{
+  static int color = 0;
+  static int index = 0;
+
+  if( index == 0)
+  {
+    color++; 
+    color = color % COLORS;
+  }
+
+  index++; 
+  index = index%NUM_LEDS_PER_STRIP;
+  
+  switch(color){
+    case 0:
+      leds[index].setRGB( 0, 200, 0);
+      //= CRGB::Chartreuse; // Green
+      break;
+    case 1:
+      leds[index].setRGB( 0, 200, 200);
+      //= CRGB::Green; // Cyan
+      break;
+    case 2:
+      leds[index].setRGB( 0, 50, 200);
+      //= CRGB::Cyan; // Blue
+      break; 
+  }
+  FastLED.show();
+}
---
+++ Evillene2/State2.ino
@@ -0,0 +1,237 @@
+#include "FastLED.h"
+
+// ColorWavesWithPalettes
+// Animated shifting color waves, with several cross-fading color palettes.
+// by Mark Kriegsman, August 2015
+//
+// Color palettes courtesy of cpt-city and its contributors:
+//   http://soliton.vm.bytemark.co.uk/pub/cpt-city/
+//
+// Color palettes converted for FastLED using "PaletteKnife" v1:
+//   http://fastled.io/tools/paletteknife/
+//
+
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+// ten seconds per color palette makes a good demo
+// 20-120 is better for deployment
+#define SECONDS_PER_PALETTE 2
+
+// Forward declarations of an array of cpt-city gradient palettes, and 
+// a count of how many there are.  The actual color palette definitions
+// are at the bottom of this file.
+extern const TProgmemRGBGradientPalettePtr gGradientPalettes[];
+extern const uint8_t gGradientPaletteCount;
+
+// Current palette number from the 'playlist' of color palettes
+uint8_t gCurrentPaletteNumber = 0;
+
+CRGBPalette16 gCurrentPalette( CRGB::Black);
+CRGBPalette16 gTargetPalette( gGradientPalettes[0] );
+
+void State2()
+{
+  EVERY_N_SECONDS( SECONDS_PER_PALETTE ) {
+    gCurrentPaletteNumber = addmod8( gCurrentPaletteNumber, 1, gGradientPaletteCount);
+    gTargetPalette = gGradientPalettes[ gCurrentPaletteNumber ];
+  }
+
+  EVERY_N_MILLISECONDS(20) {
+    nblendPaletteTowardPalette( gCurrentPalette, gTargetPalette, 16);
+  }
+  
+  colorwaves( leds, NUM_LEDS_PER_STRIP, gCurrentPalette);
+
+  FastLED.show();
+}
+
+
+// This function draws color waves with an ever-changing,
+// widely-varying set of parameters, using a color palette.
+void colorwaves( CRGB* ledarray, uint16_t numleds, CRGBPalette16& palette) 
+{
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+ 
+  uint8_t sat8 = beatsin88( 87, 220, 250);
+  uint8_t brightdepth = beatsin88( 341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
+
+  uint16_t hue16 = sHue16;//gHue * 256;
+  uint16_t hueinc16 = beatsin88(113, 300, 1500);
+  
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis ;
+  sLastMillis  = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88( 400, 5,9);
+  uint16_t brightnesstheta16 = sPseudotime;
+  
+  for( uint16_t i = 0 ; i < numleds; i++) {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
+    uint16_t h16_128 = hue16 >> 7;
+    if( h16_128 & 0x100) {
+      hue8 = 255 - (h16_128 >> 1);
+    } else {
+      hue8 = h16_128 >> 1;
+    }
+
+    brightnesstheta16  += brightnessthetainc16;
+    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+    
+    uint8_t index = hue8;
+    //index = triwave8( index);
+    index = scale8( index, 240);
+
+    CRGB newcolor = ColorFromPalette( palette, index, bri8);
+
+    uint16_t pixelnumber = i;
+    pixelnumber = (numleds-1) - pixelnumber;
+    
+    nblend( ledarray[pixelnumber], newcolor, 128);
+  }
+}
+
+// Alternate rendering function just scrolls the current palette 
+// across the defined LED strip.
+void palettetest( CRGB* ledarray, uint16_t numleds, const CRGBPalette16& gCurrentPalette)
+{
+  static uint8_t startindex = 0;
+  startindex--;
+  fill_palette( ledarray, numleds, startindex, (256 / NUM_LEDS_PER_STRIP) + 1, gCurrentPalette, 255, LINEARBLEND);
+}
+
+
+
+
+
+// Gradient Color Palette definitions for 33 different cpt-city color palettes.
+//    956 bytes of PROGMEM for all of the palettes together,
+//   +618 bytes of PROGMEM for gradient palette code (AVR).
+//  1,494 bytes total for all 34 color palettes and associated code.
+
+
+// Gradient palette "Blue_Green_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/nd/basic/tn/Blue_Green.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 12 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( Blue_Green_gp ) {
+    0,   0,  0,255,
+  127,   0, 55, 45,
+  255,   0,255,  0};
+
+// Gradient palette "es_emerald_dragon_08_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/es/emerald_dragon/tn/es_emerald_dragon_08.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 16 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( es_emerald_dragon_08_gp ) {
+    0,  97,200,  1,
+  101,  47,133,  1,
+  178,  13, 43,  1,
+  255,   2, 10,  1};
+
+// Gradient palette "Blue_Cyan_Yellow_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/nd/basic/tn/Blue_Cyan_Yellow.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( Blue_Cyan_Yellow_gp ) {
+    0,   0,  0,200,
+   63,   0, 55,200,
+  127,   0,200,200,
+  191,  42,200, 45,
+  255, 200,200,  0};
+
+
+  // Gradient palette "rainfall_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/jjg/misc/tn/rainfall.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 56 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( rainfall_gp ) {
+    0, 192,118,  3,
+   36, 192,118,  3,
+   36, 200,118, 24,
+   72, 200,118, 24,
+   72, 200,189, 37,
+  109, 200,189, 37,
+  109,  58,159, 43,
+  145,  58,159, 43,
+  145,   7,133, 52,
+  182,   7,133, 52,
+  182,   4,118, 50,
+  218,   4,118, 50,
+  218,   1, 85,  8,
+  255,   1, 85,  8};
+
+// Gradient palette "summer_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/h5/tn/summer.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 64 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( summer_gp ) {
+    0,   0, 55, 25,
+   17,   1, 62, 25,
+   33,   1, 72, 25,
+   51,   3, 82, 25,
+   68,   8, 92, 25,
+   84,  14,104, 25,
+  102,  23,115, 0,
+  119,  35,127, 0,
+  135,  48,141, 0,
+  153,  67,156, 0,
+  170,  88,169, 0,
+  186, 112,186, 0,
+  204, 142,201, 0,
+  221, 175,200, 0,
+  237, 180,180, 0,
+  255, 200,200, 0};
+
+// Gradient palette "es_emerald_dragon_12_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/es/emerald_dragon/tn/es_emerald_dragon_12.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 16 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( es_emerald_dragon_12_gp ) {
+    0,   1, 10,  1,
+  153, 104,156,  7,
+  229, 200,200,138,
+  255, 200,200,158};
+
+
+// Single array of defined cpt-city color palettes.
+// This will let us programmatically choose one based on
+// a number, rather than having to activate each explicitly 
+// by name every time.
+// Since it is const, this array could also be moved 
+// into PROGMEM to save SRAM, but for simplicity of illustration
+// we'll keep it in a regular SRAM array.
+//
+// This list of color palettes acts as a "playlist"; you can
+// add or delete, or re-arrange as you wish.
+const TProgmemRGBGradientPalettePtr gGradientPalettes[] = {
+ 
+  
+  es_emerald_dragon_08_gp,
+  summer_gp,  
+  rainfall_gp,
+  es_emerald_dragon_12_gp,
+  Blue_Cyan_Yellow_gp,
+  };
+
+
+
+// Count of how many cpt-city gradients are defined:
+const uint8_t gGradientPaletteCount = 
+  sizeof( gGradientPalettes) / sizeof( TProgmemRGBGradientPalettePtr );
